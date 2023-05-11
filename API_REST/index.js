27e4cf9bc9f146d434a4d543aadaca6139e59d59
Elleylson santtos <maxta.gamer@gmail.com>
2023-05-10 23:56:54 -0300

    update

---
+++ API_REST/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const alunos = require("./src/data/alunos.json");
+const users = require("./src/data/users.json");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.json("index");
+});
+
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/alunos", (req, res) => {
+  return res.json(alunos);
+});
+
+app.listen(3000, () => {
+  console.log("Express startd at http://localhost:3000");
+});
