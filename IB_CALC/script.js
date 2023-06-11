d23b4c6f57ddd72d13f1874bf3d03d6f625a4657
Elleylson santtos <maxta.gamer@gmail.com>
2023-06-11 01:31:05 -0300

    update

--- IB_CALC/script.js
+++ IB_CALC/script.js
@@ -1,34 +1,70 @@
 const form = document.querySelector("form");
-const cliente = document.querySelector("#cliente");
 const altura = document.querySelector("#altura");
 const ladoA = document.querySelector("#ladoA");
 const ladoB = document.querySelector("#ladoB");
 const sobra = document.querySelector("#sobra");
+const quebrados = document.querySelector("#quebrados");
+const resultado = document.querySelector("#resultado");
+const out_total = document.querySelector("#total");
 
-const clientes = [];
+let total_V = 0;
 
-const submitForm = (e) => {
-  e.preventDefault();
+let clientes = [];
 
-  const altura_V = altura.value;
-  let ladoA_V = ladoA.value;
-  let ladoB_V = ladoB.value;
+const calcFun = () => {
+  let sobra_V = Number(sobra.value);
+  let quebrados_V = Number(quebrados.value);
+
+  let isSobra = sobra_V === "" ? 0 : sobra_V;
+  let isQuebrados = quebrados_V === "" ? 0 : quebrados_V;
+
+  const altura_V = Number(altura.value);
+  let ladoA_V = Number(ladoA.value);
+  let ladoB_V = Number(ladoB.value);
   let total = ladoA_V * ladoB_V;
   let initial = 1;
-  console.log(
-    `Nivel ${initial}: ${ladoA_V} X ${ladoB_V} = ${ladoA_V * ladoB_V}`
-  );
+
+  clientes.push({
+    nivel: `Nivel ${initial < 10 ? "0" + initial : initial}`,
+    ladoA: ladoA_V < 10 ? "0" + ladoA_V : ladoA_V,
+    ladoB: ladoB_V < 10 ? "0" + ladoA_V : ladoB_V,
+    total: ladoA_V * ladoB_V,
+  });
 
   for (let i = 1; i < altura_V; i++) {
     ladoA_V--;
     ladoB_V--;
-    total += ladoA_V * ladoB_V;
-    console.log(
-      `Nivel ${(initial += 1)}: ${ladoA_V} X ${ladoB_V} = ${ladoA_V * ladoB_V}`
-    );
+    total_V += ladoA_V * ladoB_V;
+
+    clientes.push({
+      nivel: `Nivel ${(initial += 1) < 10 ? "0" + initial : initial}`,
+      ladoA: ladoA_V < 10 ? "0" + ladoA_V : ladoA_V,
+      ladoB: ladoB_V < 10 ? "0" + ladoB_V : ladoB_V,
+      total: ladoA_V * ladoB_V,
+    });
   }
 
-  console.log(total);
+  console.log(total_V);
+};
+
+const showResult = (clients) => {
+  resultado.innerHTML = "";
+  clients.forEach(({ nivel, ladoA, ladoB, total }) => {
+    resultado.innerHTML += `
+    <li>
+      <div class="left">${nivel} : ${ladoA} X ${ladoB} = </div>  
+      <div class="right"> ${total}</div>
+    </li>`;
+  });
+};
+
+const submitForm = (e) => {
+  e.preventDefault();
+  clientes = [];
+
+  calcFun();
+  showResult(clientes);
+  out_total.innerHTML += total_V;
 };
 
 form.addEventListener("submit", submitForm);
